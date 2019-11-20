65b38240f67f81733e9cc02f5cf8529372836583
zhoupeng <zhoupeng@chongaitianxia.cn>
2019-11-20 09:54:57 +0800

    feat:降低webuploader.js到0.1.5

--- dist/webuploader.js
+++ dist/webuploader.js
@@ -1,4 +1,4 @@
-/*! WebUploader 0.1.6 */
+/*! WebUploader 0.1.5 */
 
 /**
  * @fileOverview 让内部各个部件的代码可以用[amd](https://github.com/amdjs/amdjs-api/wiki/AMD)模块定义方式组织起来。
@@ -118,17 +118,9 @@
 })(window, function(window, define, require) {
   /**
    * @fileOverview jQuery or Zepto
-   * @require "jquery"
-   * @require "zepto"
    */
   define("dollar-third", [], function() {
-    var req = window.require;
-    var $ =
-      window.__dollar ||
-      window.jQuery ||
-      window.Zepto ||
-      req("jquery") ||
-      req("zepto");
+    var $ = window.__dollar || window.jQuery || window.Zepto;
 
     if (!$) {
       throw new Error("jQuery or Zepto not found!");
@@ -136,7 +128,6 @@
 
     return $;
   });
-
   /**
    * @fileOverview Dom 操作相关
    */
@@ -220,7 +211,7 @@
       /**
        * @property {String} version 当前版本号。
        */
-      version: "0.1.6",
+      version: "0.1.5",
 
       /**
        * @property {jQuery|Zepto} $ 引用依赖的jQuery或者Zepto对象。
@@ -1212,10 +1203,10 @@
       // widget直接无法监听事件，事件只能通过uploader来传递
       invoke: function(apiName, args) {
         /*
-                    {
-                        'make-thumb': 'makeThumb'
-                    }
-                 */
+                  {
+                      'make-thumb': 'makeThumb'
+                  }
+               */
         var map = this.responseMap;
 
         // 如果无API响应声明则忽略
@@ -1651,7 +1642,7 @@
     "base",
     "runtime/client",
     "lib/file"
-  ], function(Base, RuntimeClient, File) {
+  ], function(Base, RuntimeClent, File) {
     var $ = Base.$;
 
     function FilePicker(opts) {
@@ -1670,7 +1661,7 @@
       opts.button.html(opts.innerHTML);
       opts.container.html(opts.button);
 
-      RuntimeClient.call(this, "FilePicker", true);
+      RuntimeClent.call(this, "FilePicker", true);
     }
 
     FilePicker.options = {
@@ -1680,31 +1671,29 @@
       innerHTML: null,
       multiple: true,
       accept: null,
-      name: "file",
-      style: "webuploader-pick" //pick element class attribute, default is "webuploader-pick"
+      name: "file"
     };
 
-    Base.inherits(RuntimeClient, {
+    Base.inherits(RuntimeClent, {
       constructor: FilePicker,
 
       init: function() {
         var me = this,
           opts = me.options,
-          button = opts.button,
-          style = opts.style;
+          button = opts.button;
 
-        if (style) button.addClass("webuploader-pick");
+        button.addClass("webuploader-pick");
 
         me.on("all", function(type) {
           var files;
 
           switch (type) {
             case "mouseenter":
-              if (style) button.addClass("webuploader-pick-hover");
+              button.addClass("webuploader-pick-hover");
               break;
 
             case "mouseleave":
-              if (style) button.removeClass("webuploader-pick-hover");
+              button.removeClass("webuploader-pick-hover");
               break;
 
             case "change":
@@ -1831,10 +1820,10 @@
        * ```
        */
       accept: null /*{
-                title: 'Images',
-                extensions: 'gif,jpg,jpeg,bmp,png',
-                mimeTypes: 'image/*'
-            }*/
+              title: 'Images',
+              extensions: 'gif,jpg,jpeg,bmp,png',
+              mimeTypes: 'image/*'
+          }*/
     });
 
     return Uploader.register({
@@ -1852,13 +1841,13 @@
       },
 
       /**
-       * @method addBtn
+       * @method addButton
        * @for Uploader
-       * @grammar addBtn( pick ) => Promise
+       * @grammar addButton( pick ) => Promise
        * @description
        * 添加文件选择按钮，如果一个按钮不够，需要调用此方法来添加。参数跟[options.pick](#WebUploader:Uploader:options)一致。
        * @example
-       * uploader.addBtn({
+       * uploader.addButton({
        *     id: '#btnContainer',
        *     innerHTML: '选择文件'
        * });
@@ -1895,9 +1884,6 @@
           picker.once("ready", deferred.resolve);
           picker.on("select", function(files) {
             me.owner.request("add-file", [files]);
-          });
-          picker.on("dialogopen", function() {
-            me.owner.trigger("dialogOpen", picker.button);
           });
           picker.init();
 
@@ -2442,25 +2428,25 @@
       },
 
       /**
-             * 获取文件状态
-             * @return {File.Status}
-             * @example
-                     文件状态具体包括以下几种类型：
-                     {
-                         // 初始化
-                        INITED:     0,
-                        // 已入队列
-                        QUEUED:     1,
-                        // 正在上传
-                        PROGRESS:     2,
-                        // 上传出错
-                        ERROR:         3,
-                        // 上传成功
-                        COMPLETE:     4,
-                        // 上传取消
-                        CANCELLED:     5
-                    }
-             */
+           * 获取文件状态
+           * @return {File.Status}
+           * @example
+                   文件状态具体包括以下几种类型：
+                   {
+                       // 初始化
+                      INITED:     0,
+                      // 已入队列
+                      QUEUED:     1,
+                      // 正在上传
+                      PROGRESS:     2,
+                      // 上传出错
+                      ERROR:         3,
+                      // 上传成功
+                      COMPLETE:     4,
+                      // 上传取消
+                      CANCELLED:     5
+                  }
+           */
       getStatus: function() {
         return statusMap[this.id];
       },
@@ -2931,14 +2917,12 @@
           return me._addFile(file);
         });
 
-        if (files.length) {
-          me.owner.trigger("filesQueued", files);
+        me.owner.trigger("filesQueued", files);
 
-          if (me.options.auto) {
-            setTimeout(function() {
-              me.request("start-upload");
-            }, 20);
-          }
+        if (me.options.auto) {
+          setTimeout(function() {
+            me.request("start-upload");
+          }, 20);
         }
       },
 
@@ -3328,6 +3312,13 @@
        */
 
       /**
+       * @property {Object} [method='POST']
+       * @namespace options
+       * @for Uploader
+       * @description 文件上传方式，`POST`或者`GET`。
+       */
+
+      /**
        * @property {Object} [sendAsBinary=false]
        * @namespace options
        * @for Uploader
@@ -3415,7 +3406,6 @@
         this.remaning = 0;
         this.__tick = Base.bindFn(this._tick, this);
 
-        // 销毁上传相关的属性。
         owner.on("uploadComplete", function(file) {
           // 把其他块取消了。
           file.blocks &&
@@ -3463,13 +3453,11 @@
           me.request("remove-file", this);
         });
 
-        // 如果指定了开始某个文件，则只开始指定的文件。
+        // 如果指定了开始某个文件，则只开始指定文件。
         if (file) {
           file = file.id ? file : me.request("get-file", file);
 
           if (file.getStatus() === Status.INTERRUPT) {
-            file.setStatus(Status.QUEUED);
-
             $.each(me.pool, function(_, v) {
               // 之前暂停过。
               if (v.file !== file) {
@@ -3477,9 +3465,12 @@
               }
 
               v.transport && v.transport.send();
-              file.setStatus(Status.PROGRESS);
             });
-          } else if (file.getStatus() !== Status.PROGRESS) {
+
+            file.setStatus(Status.QUEUED);
+          } else if (file.getStatus() === Status.PROGRESS) {
+            return;
+          } else {
             file.setStatus(Status.QUEUED);
           }
         } else {
@@ -3489,27 +3480,28 @@
         }
 
         if (me.runing) {
-          return Base.nextTick(me.__tick);
+          return;
         }
 
         me.runing = true;
+
         var files = [];
 
         // 如果有暂停的，则续传
-        file ||
-          $.each(me.pool, function(_, v) {
-            var file = v.file;
+        $.each(me.pool, function(_, v) {
+          var file = v.file;
 
-            if (file.getStatus() === Status.INTERRUPT) {
-              me._trigged = false;
-              files.push(file);
-              v.transport && v.transport.send();
-            }
-          });
-
-        $.each(files, function() {
-          this.setStatus(Status.PROGRESS);
+          if (file.getStatus() === Status.INTERRUPT) {
+            files.push(file);
+            me._trigged = false;
+            v.transport && v.transport.send();
+          }
         });
+
+        var file;
+        while ((file = files.shift())) {
+          file.setStatus(Status.PROGRESS);
+        }
 
         file ||
           $.each(me.request("get-files", Status.INTERRUPT), function() {
@@ -3538,8 +3530,7 @@
        * @for  Uploader
        */
       stopUpload: function(file, interrupt) {
-        var me = this,
-          block;
+        var me = this;
 
         if (file === true) {
           interrupt = file;
@@ -3562,28 +3553,22 @@
           }
 
           file.setStatus(Status.INTERRUPT);
-
           $.each(me.pool, function(_, v) {
             // 只 abort 指定的文件。
-            if (v.file === file) {
-              block = v;
-              return false;
+            if (v.file !== file) {
+              return;
             }
+
+            v.transport && v.transport.abort();
+            me._putback(v);
+            me._popBlock(v);
           });
-
-          block.transport && block.transport.abort();
-
-          if (interrupt) {
-            me._putback(block);
-            me._popBlock(block);
-          }
 
           return Base.nextTick(me.__tick);
         }
 
         me.runing = false;
 
-        // 正在准备中的文件。
         if (this._promise && this._promise.file) {
           this._promise.file.setStatus(Status.INTERRUPT);
         }
@@ -4072,7 +4057,11 @@
       // 完成上传。
       _finishFile: function(file, ret, hds) {
         var owner = this.owner;
-
+        var pic_info =
+          ret.result + "," + file._info.width + "," + file._info.height;
+        $(".btns").append(
+          $("<input type='hidden' name='pic_info[]' value='" + pic_info + "'/>")
+        );
         return owner
           .request("after-send-file", arguments, function() {
             file.setStatus(Status.COMPLETE);
@@ -4108,7 +4097,6 @@
       }
     });
   });
-
   /**
    * @fileOverview 各种验证，包括文件总大小是否超出、单文件是否超出和文件是否重复。
    */
@@ -4655,9 +4643,6 @@
           data = dataTransfer.getData("text/html");
         } catch (err) {}
 
-        me.dndOver = false;
-        me.elem.removeClass(prefix + "over");
-
         if (data) {
           return;
         }
@@ -4671,6 +4656,8 @@
           );
         });
 
+        me.dndOver = false;
+        me.elem.removeClass(prefix + "over");
         return false;
       },
 
@@ -4860,14 +4847,11 @@
           mouseHandler;
 
         input.attr("type", "file");
-        // input.attr( 'capture', 'camera');
         input.attr("name", opts.name);
         input.addClass("webuploader-element-invisible");
 
-        label.on("click", function(e) {
+        label.on("click", function() {
           input.trigger("click");
-          e.stopPropagation();
-          owner.trigger("dialogopen");
         });
 
         label.css({
@@ -4933,7 +4917,6 @@
       }
     });
   });
-
   /**
    * Terms:
    *
@@ -5525,41 +5508,41 @@
    */
   define("runtime/html5/jpegencoder", [], function(require, exports, module) {
     /*
-          Copyright (c) 2008, Adobe Systems Incorporated
-          All rights reserved.
-    
-          Redistribution and use in source and binary forms, with or without
-          modification, are permitted provided that the following conditions are
-          met:
-    
-          * Redistributions of source code must retain the above copyright notice,
-            this list of conditions and the following disclaimer.
-    
-          * Redistributions in binary form must reproduce the above copyright
-            notice, this list of conditions and the following disclaimer in the
-            documentation and/or other materials provided with the distribution.
-    
-          * Neither the name of Adobe Systems Incorporated nor the names of its
-            contributors may be used to endorse or promote products derived from
-            this software without specific prior written permission.
-    
-          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-          IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-          THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-          PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-          CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-          EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-          PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-          PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-          LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-          NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-          SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        */
+        Copyright (c) 2008, Adobe Systems Incorporated
+        All rights reserved.
+
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted provided that the following conditions are
+        met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions and the following disclaimer.
+
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+
+        * Neither the name of Adobe Systems Incorporated nor the names of its
+          contributors may be used to endorse or promote products derived from
+          this software without specific prior written permission.
+
+        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+        IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+        THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+        PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+        CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+        EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+        PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+        PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+        LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+        NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+        SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+      */
     /*
-        JPEG encoder ported to JavaScript and optimized by Andreas Ritter, www.bytestrom.eu, 11/2009
-    
-        Basic GUI blocking jpeg encoder
-        */
+      JPEG encoder ported to JavaScript and optimized by Andreas Ritter, www.bytestrom.eu, 11/2009
+
+      Basic GUI blocking jpeg encoder
+      */
 
     function JPEGEncoder(quality) {
       var self = this;
@@ -6703,10 +6686,10 @@
               b = imageData[p++];
 
               /* // calculate YUV values dynamically
-                            YDU[pos]=((( 0.29900)*r+( 0.58700)*g+( 0.11400)*b))-128; //-0x80
-                            UDU[pos]=(((-0.16874)*r+(-0.33126)*g+( 0.50000)*b));
-                            VDU[pos]=((( 0.50000)*r+(-0.41869)*g+(-0.08131)*b));
-                            */
+                          YDU[pos]=((( 0.29900)*r+( 0.58700)*g+( 0.11400)*b))-128; //-0x80
+                          UDU[pos]=(((-0.16874)*r+(-0.33126)*g+( 0.50000)*b));
+                          VDU[pos]=((( 0.50000)*r+(-0.41869)*g+(-0.08131)*b));
+                          */
 
               // use lookup table (slightly faster)
               YDU[pos] =
@@ -6883,8 +6866,6 @@
             height: this.height
           };
 
-          //debugger;
-
           // 读取meta信息。
           if (!me._metas && "image/jpeg" === me.type) {
             Util.parseMeta(me._blob, function(error, ret) {
@@ -7028,12 +7009,12 @@
       meta: function(val) {
         // setter
         if (val) {
-          this._metas = val;
+          this._meta = val;
           return this;
         }
 
         // getter
-        return this._metas;
+        return this._meta;
       },
 
       destroy: function() {
@@ -7306,7 +7287,6 @@
       })()
     });
   });
-
   /**
    * @fileOverview Transport
    * @todo 支持chunked传输，优势：
@@ -7498,10 +7478,10 @@
      */
 
     /* this function is much faster,
-          so if possible we use it. Some IEs
-          are the only ones I know of that
-          need the idiotic second function,
-          generated by an if clause.  */
+        so if possible we use it. Some IEs
+        are the only ones I know of that
+        need the idiotic second function,
+        generated by an if clause.  */
     var add32 = function(a, b) {
         return (a + b) & 0xffffffff;
       },
@@ -8502,17 +8482,15 @@
             //     me._responseJson = xhr.exec('getResponseAsJson');
             // } catch ( error ) {
 
-            p = function(s) {
-              try {
-                if (window.JSON && window.JSON.parse) {
-                  return JSON.parse(s);
+            p =
+              (window.JSON && window.JSON.parse) ||
+              function(s) {
+                try {
+                  return new Function("return " + s).call();
+                } catch (err) {
+                  return {};
                 }
-
-                return new Function("return " + s).call();
-              } catch (err) {
-                return {};
-              }
-            };
+              };
             me._responseJson = me._response ? p(me._response) : {};
 
             // }
@@ -8541,7 +8519,6 @@
       }
     });
   });
-
   /**
    * @fileOverview Blob Html实现
    */
@@ -8553,7 +8530,7 @@
       slice: function(start, end) {
         var blob = this.flashExec("Blob", "slice", start, end);
 
-        return new Blob(this.getRuid(), blob);
+        return new Blob(blob.uid, blob);
       }
     });
   });
